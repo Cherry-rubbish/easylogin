a9fc4eb4c941e75104331bd2191840ace4644b6a
Cherry-rubbish <oerabokurabit2018@gmail.com>
2022-11-23 17:22:28 +0900

    easylogin

---
+++ easylogin/easylogin/src/main/java/com/example/easylogin/easyloginApplication.java
@@ -0,0 +1,13 @@
+package easylogin.src.main.java.com.example.easylogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class easyloginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(easyloginApplication.class, args);
+	}
+
+}
